bb51cb31f065bd895c3badad50d48d34e7a9498e
Md Toriqul Islam <toriqul.int@gmail.com>
2025-01-05 02:03:32 +0800

    Update python3 solution for Maximum Difference Between Node and Ancestor

--- medium/1092-maximum-difference-between-node-and-ancestor/solution.py
+++ medium/1092-maximum-difference-between-node-and-ancestor/solution.py
@@ -1,6 +1,6 @@
-# Maximum Difference Between Node and Ancestor
+# 1092. Maximum Difference Between Node and Ancestor
 # Difficulty: Medium
-# Runtime: 38 ms
+# Runtime: 37 ms
 # Memory: 19.1 MB
 
 # Definition for a binary tree node.
@@ -11,21 +11,19 @@
 #         self.right = right
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        
         m = [0]
-        self.dfs(root, m)
+        self.tree(root, m)
         return m[0]
 
-    def dfs(self, root, m):
+    def tree(self, root, m):
         if not root:
             return float('inf'), float('-inf')
 
-        left = self.dfs(root.left, m)
-        right = self.dfs(root.right, m)
-
+        left = self.tree(root.left, m)
+        right = self.tree(root.right, m)
         min_val = min(root.val, min(left[0], right[0]))
         max_val = max(root.val, max(left[1], right[1]))
-
         m[0] = max(m[0], max(abs(min_val - root.val), abs(max_val - root.val)))
-
         return min_val, max_val
         
