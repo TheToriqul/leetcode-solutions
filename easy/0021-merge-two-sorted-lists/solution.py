ddc6fc719a6098a1ad46671368688fdbbc756b02
Md Toriqul Islam <toriqul.int@gmail.com>
2025-01-05 02:09:21 +0800

    Update python3 solution for Merge Two Sorted Lists

--- easy/0021-merge-two-sorted-lists/solution.py
+++ easy/0021-merge-two-sorted-lists/solution.py
@@ -1,7 +1,7 @@
 # 21. Merge Two Sorted Lists
 # Difficulty: Easy
-# Runtime: 43 ms
-# Memory: 17.4 MB
+# Runtime: 41 ms
+# Memory: 17.3 MB
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -12,17 +12,17 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
         temp = ListNode()
-        head = temp
+        tail = temp
 
         while list1 is not None and list2 is not None:
             if list1.val <= list2.val:
-                head.next = list1
+                tail.next = list1
                 list1 = list1.next
             else:
-                head.next = list2
+                tail.next = list2
                 list2 = list2.next
             
-            head= head.next
+            tail= tail.next
 
-        head.next = list2 if list1 is None else list1
+        tail.next = list2 if list1 is None else list1
         return temp.next
